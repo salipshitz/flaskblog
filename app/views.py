85412b69ca4761bd15128cc9fec66152c0c1c0ef
Sa'ar <geger40.1@gmail.com>
2017-10-14 12:43:42 -0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
+current_user = None
+
 users = [
     {'nickname': 'Mark Anthony',
      'path': '/users/VivirMiVida',
@@ -40,11 +42,10 @@ posts = [
     
 
 @app.route("/")
-@app.route("/home")
+@app.route("/index")
 def home():
-    user = users[1]
     return render_template('index.html',
-                           user=user,
+                           user=current_user,
                            pg='/users',
                            link_text='Users',
                            posts=posts)
@@ -58,6 +59,10 @@ def users():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for openid = "%s", remember_me = %s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect("/index")
     return render_template('login.html',
                            title='Sign in',
                            form=form)
