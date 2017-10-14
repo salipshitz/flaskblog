dfb14afe6ba21fe01142d3f6ee09108ff1dc3020
Sa'ar <geger40.1@gmail.com>
2017-10-14 11:33:42 -0700

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,56 @@
+from flask import render_template
+from app import app
+
+users = [
+    {'nickname': 'Mark Anthony',
+     'path': '/users/VivirMiVida',
+     'account_name': '@VivirMiVida'},
+    {'nickname': 'Julian CremeSalad',
+     'path': '/users/jcream',
+     'account_name': '@jcream'},
+    {'nickname': 'But Brutus is an honourable man.',
+     'path': '/users/realBrutalBrutus',
+     'account_name': '@realBrutalBrutus'},
+    {'nickname': 'Queso-us',
+     'path': '/users/NotCassius',
+     'account_name': '@NotCassius'},
+    {'nickname': 'John Pizza',
+     'path': '/users/JoPizza',
+     'account_name': '@JoPizza'}
+]
+posts = [
+    {
+        'author': users[0],
+        'body': 'Voy a reir, voy a bailar when Julian CremeSalad is the king'
+    },
+    {
+        'author': users[3],
+        'body': 'Stab dat salad 23 time bro!'
+    },
+    {
+        'author': users[0],
+        'body': 'Friends, salads, croutonmen, lend me your ears. I come to bury CremeSalad, not to praise him, cause Brutal is an honourable man.'
+    },
+    {
+        'author': users[4],
+        'body': 'Climate change is a hoax and the Antarctican government is torching the ice caps'
+    }
+]
+    
+
+@app.route("/")
+@app.route("/home")
+def home():
+    user = users[1]
+    return render_template('index.html',
+                           user=user,
+                           pg='/users',
+                           link_text='Users',
+                           posts=posts)
+@app.route("/users")
+def pg2():
+    return render_template('members.html',
+                           title='Users',
+                           index='/index',
+                           users=users)
+ 
